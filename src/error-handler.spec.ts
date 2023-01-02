af3e695ab91294144921975e3989e7fe61d401db
Pascal Garber <pascal@artandcode.studio>
2023-01-02 15:44:07 +0100

    fix(error-handler): Fix uncaught exception test

--- src/error-handler.spec.ts
+++ src/error-handler.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, assert, spy, beforeEach, afterEach, on } from '@gjsify/unit';
+import { describe, it, expect, assert, spy, beforeEach, afterEach, on } from '@gjsify/unit';
 
 import { Event, EventTarget, setWarningHandler } from "./index.js"
 import process from 'process';
@@ -47,23 +47,24 @@ export const ErrorHandlerTest = async () => {
             })
         })
 
-        // TODO Fix Gjs
-        await on(["Deno", "Node.js"], async () => {
-            await it("should emit an uncaughtException event if a listener threw an error", async () => {
-                const f = spy(_event => {})
-                const target = new EventTarget()
-                const error = new Error("test error")
-                target.addEventListener("foo", () => {
-                    throw error
-                })
-
-                process.on("uncaughtException", f)
-                target.dispatchEvent(new Event("foo"))
-                process.removeListener("uncaughtException", f)
-
-                assert.strictEqual(f.calls.length, 1, "f should be called.")
-                assert.strictEqual(f.calls[0].arguments[0], error)
+        await it("should emit an uncaughtException event if a listener threw an error", async () => {
+            const onUncaughtException = spy(_event => {})
+            const target = new EventTarget()
+            const error = new Error("test error")
+            target.addEventListener("foo", () => {
+                throw error
             })
+
+            process.on("uncaughtException", onUncaughtException)
+            target.dispatchEvent(new Event("foo"))
+            process.removeListener("uncaughtException", onUncaughtException)
+
+            assert.strictEqual(onUncaughtException.calls.length, 1, "f should be called.")
+
+            // TODO this are currently not the same objects, see https://gitlab.gnome.org/GNOME/gjs/-/issues/523
+            // assert.strictEqual(onUncaughtException.calls[0].arguments[0], error)
+            expect(onUncaughtException.calls[0].arguments[0].message).toBe(error.message)
+            expect(onUncaughtException.calls[0].arguments[0].stack?.trim()).toBe(error.stack?.trim())
         })
     })
 }
